d358bf30d70b9aba3a42cd1408cba8c3838f2b85
ttthkim-hue <ttthkim@gmail.com>
2026-08-31 21:59:56 -0700

    Add Grok glance color

--- android/app/src/main/java/com/quotaedge/galaxy/ui/theme/Color.kt
+++ android/app/src/main/java/com/quotaedge/galaxy/ui/theme/Color.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val ClaudeOrange = Color(0xFFD97757)
 val CodexGreen = Color(0xFF10A37F)
+val GrokGray = Color(0xFFE8E8EA)
 val BgDark = Color(0xFF010101)
 val SurfaceDark = Color(0xFF17171A)
 val TextMuted = Color(0xFF8E8E93)
